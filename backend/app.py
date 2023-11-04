b7485afff7429d3e336db7cf351086b6f2cf97f5
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-11-04 17:42:28 +0100

    feat: add a connection between the flask app and mongodb

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,12 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+
+MONGODB_URI = os.environ.get("MONGODB_ENDPOINT")
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = MONGODB_URI
+mongo = PyMongo(app)
 
 @app.route('/')
 def hello_world():
