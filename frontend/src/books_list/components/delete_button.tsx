fa82306cc68102553db6c042608642254a8c8651
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-11-06 12:54:55 +0100

    refactor: cleanup the DeleteButton implementation by using the IconButton component

--- frontend/src/books_list/components/delete_button.tsx
+++ frontend/src/books_list/components/delete_button.tsx
@@ -1,21 +1,7 @@
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import {
-    IconLookup,
-    IconDefinition,
-    findIconDefinition
-  } from '@fortawesome/fontawesome-svg-core'
-import Button from 'react-bootstrap/Button';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import IconButton from './icon_buton';
 
-library.add(fas)
-const trashLookup: IconLookup = { prefix: 'fas', iconName: 'trash' }
-const trashIconDefinition: IconDefinition = findIconDefinition(trashLookup)
-
-export default function DeleteButton({ onClick }: { onClick: Function }) {
+export default function DeleteButton({ onClick }: { onClick: () => any }) {
     return (
-        <Button onClick={() => onClick()}>
-            <FontAwesomeIcon icon={trashIconDefinition} />
-        </Button>
+        <IconButton iconName='trash' onClick={() => onClick()}/>
     )
 }
