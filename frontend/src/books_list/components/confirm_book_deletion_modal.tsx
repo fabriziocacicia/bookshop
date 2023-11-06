d1a2bfba21bc4c0b8fca6b1a4ba51b261a72d24c
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-11-06 12:45:54 +0100

    feat: add the ui for the delete book button and for the delete confirmation modal

---
+++ frontend/src/books_list/components/confirm_book_deletion_modal.tsx
@@ -0,0 +1,21 @@
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+
+export default function ConfirmBookDeletionModal({bookTitle, show, onHide, onClickCancel, onClickDelete}: {bookTitle: string, show: boolean, onHide: () => any, onClickCancel: () => any, onClickDelete: () => any}) {
+    return (
+        <Modal show={show} onHide={() => onHide()}>
+            <Modal.Header closeButton>
+                <Modal.Title>Delete Book</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>Are you sure you want to delete the book {bookTitle}?</Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary" onClick={() => onClickCancel()}>
+                    Cancel
+                </Button>
+                <Button variant="primary" onClick={() => onClickDelete()}>
+                    Delete
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    )
+}
--- frontend/src/books_list/page.tsx
+++ frontend/src/books_list/page.tsx
@@ -1,4 +1,8 @@
 import Table from 'react-bootstrap/Table';
+import DeleteButton from './components/delete_button';
+import { useState } from 'react';
+import ConfirmBookDeletionModal from './components/confirm_book_deletion_modal';
+
 
 const books = [
     { _id: "1", title: 'book1', author: 'author', year: 1230, price: 20.0},
@@ -6,7 +10,7 @@ const books = [
     { _id: "3", title: 'book3', author: 'author', year: 2002, price: 40.5},
 ]
 
-function BookListItem({id, title, author, year, price }: {id: string, title: string, author: string, year: number, price: number}) {
+function BookListItem({id, title, author, year, price, onClickDelete }: {id: string, title: string, author: string, year: number, price: number, onClickDelete: Function}) {
     return (
         <tr>
             <td>{id}</td>
@@ -14,30 +18,47 @@ function BookListItem({id, title, author, year, price }: {id: string, title: str
             <td>{author}</td>
             <td>{year}</td>
             <td>{price}€</td>
+            <td>
+                <DeleteButton onClick={onClickDelete} />
+            </td>
         </tr> 
     );
 }
 
 
 export default function BooksListPage() {
+    const [confirmBookDeletionModalShowState, setConfirmBookDeletionModalShowState] = useState<boolean>(false);
+    const [bookToDeleteTitle, setbookToDeleteTitle] = useState<string>("")
+
+    const handleCloseConfirmBookDeletionModal = () => setConfirmBookDeletionModalShowState(false);
+    const handleShowConfirmBookDeletionModal = (bookTitle: string) => {
+        setbookToDeleteTitle(bookTitle);
+        setConfirmBookDeletionModalShowState(true);
+    };
+
+
     const listItems = books.map(book => 
-            <BookListItem id={book._id} title={book.title} author={book.author} year={book.year} price={book.price} />
+            <BookListItem key={book._id} id={book._id} title={book.title} author={book.author} year={book.year} price={book.price} onClickDelete={() => handleShowConfirmBookDeletionModal(book.title)}/>
         );
         
     return (
-        <Table responsive>
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Title</th>
-                    <th>Author</th>
-                    <th>Year</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                {listItems}
-            </tbody>
-        </Table>
+        <>  
+            <ConfirmBookDeletionModal bookTitle={bookToDeleteTitle} show={confirmBookDeletionModalShowState} onHide={handleCloseConfirmBookDeletionModal} onClickCancel={handleCloseConfirmBookDeletionModal} onClickDelete={()=>{}}/>
+            <Table responsive>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Author</th>
+                        <th>Year</th>
+                        <th>Price</th>
+                        <th>Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {listItems}
+                </tbody>
+            </Table>
+        </>
     )
 }
