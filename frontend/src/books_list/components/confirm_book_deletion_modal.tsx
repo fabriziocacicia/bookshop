8619ae0035e36279451911849c0543f1cf2ef6dc
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-11-07 14:06:34 +0100

    feat: add a visual feedback to the ConfirmBookDeletionModal's Delete button

--- frontend/src/books_list/components/confirm_book_deletion_modal.tsx
+++ frontend/src/books_list/components/confirm_book_deletion_modal.tsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 export default function ConfirmBookDeletionModal({bookTitle, show, onHide, onClickCancel, onClickDelete}: {bookTitle: string, show: boolean, onHide: () => any, onClickCancel: () => any, onClickDelete: () => any}) {
+    const [deleting, setDeleting] = useState<boolean>(false);
+
+    const handleClickDelete = () => {
+        setDeleting(true);
+        onClickDelete()
+    }
+
     return (
         <Modal show={show} onHide={() => onHide()}>
             <Modal.Header closeButton>
@@ -12,8 +20,8 @@ export default function ConfirmBookDeletionModal({bookTitle, show, onHide, onCli
                 <Button variant="secondary" onClick={() => onClickCancel()}>
                     Cancel
                 </Button>
-                <Button variant="primary" onClick={() => onClickDelete()}>
-                    Delete
+                <Button variant="primary" onClick={() => handleClickDelete()}>
+                    {deleting? "Deleting..." : "Delete"}
                 </Button>
             </Modal.Footer>
         </Modal>
