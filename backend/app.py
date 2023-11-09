9bbc466d14a6137e220a9242b00f592bf7e1bbcc
Fabrizio Cacicia <mail@fabriziocacicia.com>
2023-11-09 09:36:16 +0100

    feat: add the ai search functionality to the backend

--- backend/app.py
+++ backend/app.py
@@ -3,6 +3,7 @@ from flask import Flask, request, jsonify
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 from flask_cors import CORS
+from mongoquery import MongoQuery
 
 MONGODB_URI = os.environ.get("MONGODB_ENDPOINT")
 
@@ -10,6 +11,9 @@ app = Flask(__name__)
 app.config["MONGO_URI"] = MONGODB_URI
 mongo = PyMongo(app)
 db = mongo.db
+
+keys = ['_id', 'index', 'title', 'author', 'year', 'price']
+queryfier = MongoQuery(keys, 'books')
 
 CORS(app)
 
@@ -102,6 +106,44 @@ def deleteBook(book_id):
         status='OK',
         message='Book deleted successfully',
         deleted_book_id=book_id
+    ), 200
+    
+def _preprocess_ai_generated_query(query: str) -> str:
+    query = query.split('}, {')[0]
+    query += "})"
+    
+    return query
+
+def _execute_ai_generated_query(query: str) -> []:
+    local_vars = {}
+    global_vars = {}
+    global_vars['db'] = db
+    line = 'result = db.books.find({"year": {"$gt": 100}, "year": {"$lt": 1900}})'
+    line = 'result = {}'.format(query)
+    exec(line, global_vars, local_vars)
+    books = local_vars.get('result')
+    
+    return books
+    
+@app.route('/aisearch', methods=['POST'])
+def aiSearch():
+    data = request.get_json(force=True)
+    
+    query = queryfier.generate_query('''{}'''.format(data['query']))
+    query = _preprocess_ai_generated_query(query)
+
+    books = _execute_ai_generated_query(query)
+    data = []
+    
+    if books is not None:
+        for book in books:
+            if '_id' in book:
+                book['_id'] = str(book['_id'])
+            data.append(book)
+    
+    return jsonify(
+        books=data,
+        query=query
     ), 200
 
 if __name__ == "__main__":
